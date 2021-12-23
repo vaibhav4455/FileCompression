1302100c7952905aa2a0bd08dfa0151168aeab08
vaibhav4455 <vaibhav661998@gmail.com>
2021-12-23 22:08:02 +0530

    file compression working

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Compressor from "./Components/Compressor";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Compressor />
+  </div>
+  
+
+  );
+}
+export default App;
